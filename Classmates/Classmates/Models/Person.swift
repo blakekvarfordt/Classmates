fe4552792141652d6c3d5c88e9e7a643fd57b91e
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-11-07 21:05:06 -0700

    creates Person model

---
+++ Classmates/Classmates/Models/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  Classmates
+//
+//  Created by Blake kvarfordt on 11/7/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
